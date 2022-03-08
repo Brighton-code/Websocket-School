0e728575fb4ca255a5ad167a4552ec327de6e1b6
Brighton <81828779+Brighton-code@users.noreply.github.com>
2022-03-08 20:19:43 +0100

    Update server.js
    Skeleton websocket server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("I changed a file");
+const PORT = 8000;
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: PORT });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(data) {
+    console.log(JSON.parse(`${data}`));
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(`${data}`);
+      }
+    });
+  });
+});
