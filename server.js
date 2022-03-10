4896bbc60e81baea83d0d2a54f3218fc33a5a827
Brighton <81828779+Brighton-code@users.noreply.github.com>
2022-03-10 09:52:09 +0100

    Update server.js
    Test with user data on connect

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const PORT = 8000;
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: PORT });
+const wss = new WebSocket.Server({
+  port: PORT,
+  clientTracking: true,
+});
 
 wss.on("connection", function connection(ws) {
+  ws.user = {
+    userData: {
+      name: "Brighton",
+      nickName: "TheHyper",
+    },
+    UUID: "0002-2218",
+  };
+  console.log(ws.user);
   ws.on("message", function incoming(data) {
     console.log(JSON.parse(`${data}`));
     wss.clients.forEach(function each(client) {
