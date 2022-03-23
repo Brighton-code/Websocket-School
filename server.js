de18773c6c59293d22e650a5d1ff4c172e2b3e11
Brighton <81828779+Brighton-code@users.noreply.github.com>
2022-03-23 11:00:51 +0100

    Update server.js
    Small changes

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const PORT = 8000;
-const WebSocket = require("ws");
+const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({
-  port: PORT,
-  clientTracking: true,
+	port: PORT,
+	clientTracking: true,
 });
+console.log(`Listening on port: ${PORT}`);
 
-wss.on("connection", function connection(ws) {
-  ws.user = {
-    userData: {
-      name: "Brighton",
-      nickName: "TheHyper",
-    },
-    UUID: "0002-2218",
-  };
-  console.log(ws.user);
-  ws.on("message", function incoming(data) {
-    console.log(JSON.parse(`${data}`));
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(`${data}`);
-      }
-    });
-  });
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(data) {
+		console.log(JSON.parse(`${data}`));
+		wss.clients.forEach(function each(client) {
+			if (client !== ws && client.readyState === WebSocket.OPEN) {
+				client.send(`${data}`);
+			}
+		});
+	});
 });
